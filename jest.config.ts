6196ba243f68bbb016df93d8fb9b40639cdc3383
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-12 10:59:19 -0800

    Fully Migrate to jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,9 +6,6 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-<<<<<<< HEAD
   testEnvironment: "jsdom"
-=======
->>>>>>> 9a1e3ae (New config and test utils)
 };
 export default config;
