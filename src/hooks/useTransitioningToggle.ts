15a0c87cb20df399efe2eb69deed7ecf8532da50
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-11 18:32:15 -0800

    Fix useTransitioningToggle signature

--- src/hooks/useTransitioningToggle.ts
+++ src/hooks/useTransitioningToggle.ts
@@ -30,25 +30,27 @@ export type TransitioningToggleState = readonly [
  * Hook to create a boolean toggle state that animates when toggled
  * @typeParam E the Animated HTMLElement
  * @param initialState
- * @param initialTransitioning if provided, will toggle away from initialState when the togglingElementRef is first set
+ * @param initialTransitioning if true, will toggle away from initialState when the togglingElementRef is first set
  *
  * @return [isToggled, toggleCallback, togglingElementRef, currentAnimation]
  */
 function useTransitioningToggle(
     initialState = false,
-    initialTransitioning: ToggleTransitions | null = null
+    initialTransitioning: boolean = false
 ): TransitioningToggleState {
     const [isToggled, startTransition, endTransition, isTransitioning] = useTransitioningState<
         boolean,
         ToggleTransitions
-    >(initialState, initialTransitioning);
+    >(initialState, initialTransitioning ? nextTransition(initialState) : null);
 
-    const nextTransition = isToggled ? 'togglingOff' : 'togglingOn';
+    const transition = nextTransition(isToggled);
 
-    const startToggling = useCallback(() => startTransition((prev) => !prev, nextTransition), [nextTransition]);
+    const startToggling = useCallback(() => startTransition((prev) => !prev, transition), [transition]);
 
-    const currentTransition = isTransitioning !== null ? nextTransition : null;
+    const currentTransition = isTransitioning !== null ? transition : null;
     return [isToggled, startToggling, endTransition, currentTransition];
 }
+
+const nextTransition = (isToggled: boolean): ToggleTransitions => isToggled ? 'togglingOff' : 'togglingOn';
 
 export default useTransitioningToggle;
