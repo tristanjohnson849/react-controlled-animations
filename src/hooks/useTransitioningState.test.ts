85906cc1378308fb274f0ba4a306b205ca8595fd
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-12 20:13:51 -0800

    Define cases for useTransitioningState

---
+++ src/hooks/useTransitioningState.test.ts
@@ -0,0 +1,33 @@
+// import { act, renderHook } from '@testing-library/react-hooks';
+
+// import useTransitioningState from './useTransitioningState';
+
+// // initial state
+// test('initialState=undefined initialTransition=undefined => get undefined state, transition=null', () => {});
+
+// test('initialState=defined initialTransition=undefined => get defined state, transition=null', () => {});
+
+// test('initialState=undefined initialTransition=defined => get undefined state, transition=defined', () => {});
+
+// test('initialState=defined initialTransition=defined => get defined state, transition=defined', () => {});
+
+// test('initialTransition=defined endTransition => get initial state, transition=null', () => {});
+
+// // active state
+// test('transition=null startTransition on null => state=new, transition=null', () => {});
+
+// test('transition=null startTransition on new => state unchanged, transition=new', () => {});
+
+// test('transition=null startTransition on new endTransition => state=new, transition=null', () => {});
+
+// test('transition=null endTransition => no changes', () => {});
+
+// test('transition=defined startTransition on null => state=new, transition=null', () => {});
+
+// test('transition=defined startTransition on new => no changes', () => {});
+
+// test('transition=defined startTransition on same transition diff state => no changes', () => {});
+
+// test('transition=defined startTransition on same transition diff state type=value endTransition => state=last', () => {});
+
+// test('transition=defined startTransition on same transition diff state type=callback endTransition => state=aggregate', () => {});
