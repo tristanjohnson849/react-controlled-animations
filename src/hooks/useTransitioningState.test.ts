49045dab5cffdd0ecad9151e48f53ead4988b722
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-12 23:30:42 -0800

    Initial state tests

--- src/hooks/useTransitioningState.test.ts
+++ src/hooks/useTransitioningState.test.ts
@@ -1,17 +1,57 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import useTransitioningState from './useTransitioningState';
 
 // import useTransitioningState from './useTransitioningState';
 
-// // initial state
-// test('initialState=undefined initialTransition=undefined => get undefined state, transition=null', () => {});
+// initial state
+test('initialState=undefined initialTransition=undefined => get undefined state, transition=null', () => {
+    const { result } = renderHook(() => useTransitioningState());
+    const [state, , , currentTransition] = result.current;
 
-// test('initialState=defined initialTransition=undefined => get defined state, transition=null', () => {});
+    expect(state).toBeUndefined();
+    expect(currentTransition).toBeNull();
+});
 
-// test('initialState=undefined initialTransition=defined => get undefined state, transition=defined', () => {});
+test('initialState=defined initialTransition=undefined => get defined state, transition=null', () => {
+    const { result } = renderHook(() => useTransitioningState('initial'));
+    const [state, , , currentTransition] = result.current;
 
-// test('initialState=defined initialTransition=defined => get defined state, transition=defined', () => {});
+    expect(state).toBe('initial');
+    expect(currentTransition).toBeNull();
+});
 
-// test('initialTransition=defined endTransition => get initial state, transition=null', () => {});
+test('initialState=undefined initialTransition=defined => get undefined state, transition=defined', () => {
+    const { result } = renderHook(() => useTransitioningState(undefined, 'initialTransition'));
+    const [state, , , currentTransition] = result.current;
+
+    expect(state).toBeUndefined();
+    expect(currentTransition).toBe('initialTransition');
+});
+
+test('initialState=defined initialTransition=defined => get defined state, transition=defined', () => {
+    const { result } = renderHook(() => useTransitioningState('initial', 'initialTransition'));
+    const [state, , , currentTransition] = result.current;
+
+    expect(state).toBe('initial');
+    expect(currentTransition).toBe('initialTransition');
+});
+
+test('initialTransition=defined endTransition => get initial state, transition=null', () => {
+    const { result } = renderHook(() => useTransitioningState('initial', 'initialTransition'));
+
+    const [startingState, , endStartingTransition, startingTransition] = result.current;
+
+    expect(startingState).toBe('initial');
+    expect(startingTransition).toBe('initialTransition');
+
+    act(() => endStartingTransition());
+
+    const [endingState, , , endingTransition] = result.current;
+
+    expect(endingState).toBe('initial');
+    expect(endingTransition).toBeNull();
+});
 
 // // active state
 // test('transition=null startTransition on null => state=new, transition=null', () => {});
