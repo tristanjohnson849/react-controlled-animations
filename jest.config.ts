1c7660be55326f5db6547c6083f409e06e672a60
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-13 19:54:45 -0800

    New config and test utils

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,9 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+<<<<<<< HEAD
   testEnvironment: "jsdom"
+=======
+>>>>>>> 9a1e3ae (New config and test utils)
 };
 export default config;
