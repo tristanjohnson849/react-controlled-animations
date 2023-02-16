46d278814989097e044e1f667bb9b72df959f52b
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-16 16:18:25 -0600

    Set up module config

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common';
-export { default as ControlledAnimated } from './ControlledAnimated';
-export * from './ControlledAnimated';
-export { default as HoverAnimated } from './HoverAnimated';
-export * from './HoverAnimated';
