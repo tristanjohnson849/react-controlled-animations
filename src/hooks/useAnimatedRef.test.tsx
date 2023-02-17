2d55c02e6a09b4a8d0812e192ed64e1bd1523d37
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-17 00:20:21 -0600

    Fix test config

--- src/hooks/useAnimatedRef.test.tsx
+++ src/hooks/useAnimatedRef.test.tsx
@@ -11,14 +11,14 @@ afterAll(cleanup);
 const Animated = ({ A = [], B = [], onAnimationEnd }: { 
     A?: AnimationInput, 
     B?: AnimationInput,
-    onAnimationEnd: (completedAnimation: 'A', webAnimation: Animation) => void
+    onAnimationEnd: (completedAnimation: 'A' | 'B' | null, webAnimation: Animation | null) => void
 }) => {
     const [state, setState] = useState<'A' | 'B' | null>(null); 
 
     const ref = useAnimatedRef<'A' | 'B', HTMLDivElement>(
         state,
         { A, B },
-        (completedAnimation: 'A', webAnimation: Animation) => {
+        (completedAnimation: 'A' | 'B' | null, webAnimation: Animation | null) => {
             onAnimationEnd(completedAnimation, webAnimation);
         }
     );
