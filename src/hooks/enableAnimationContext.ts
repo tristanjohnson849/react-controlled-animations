692046c2101b01214bd21975a5740e84bd719fc5
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-26 18:53:22 -0800

    Export Context interface and add doc

--- src/hooks/enableAnimationContext.ts
+++ src/hooks/enableAnimationContext.ts
@@ -1,10 +1,15 @@
 import React from 'react';
 
-interface EnableAnimationContext {
+export interface EnableAnimationContext {
     enabled: boolean;
     setEnabled?: (enabled: boolean) => void;
 }
 
+/**
+ * React.Context to enable or disable all animations under the Provider.
+ * 
+ * The context is used by {@link  hooks.useAnimatedRef} to immediately end an animation after starting
+ */
 export const EnableAnimationContext = React.createContext<EnableAnimationContext>({
     enabled: true,
 });
