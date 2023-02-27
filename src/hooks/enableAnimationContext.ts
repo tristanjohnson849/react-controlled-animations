d9493b6481b7bc9aec4e68d0e330ff537b299a9a
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-26 18:55:56 -0800

    Lint fix

--- src/hooks/enableAnimationContext.ts
+++ src/hooks/enableAnimationContext.ts
@@ -7,7 +7,7 @@ export interface EnableAnimationContext {
 
 /**
  * React.Context to enable or disable all animations under the Provider.
- * 
+ *
  * The context is used by {@link  hooks.useAnimatedRef} to immediately end an animation after starting
  */
 export const EnableAnimationContext = React.createContext<EnableAnimationContext>({
